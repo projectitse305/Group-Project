252273a922c3fc65377a6cd6a7a82b8abe17195b
projectitse305 <147308057+projectitse305@users.noreply.github.com>
2023-12-12 09:59:09 +0300

    Update FillForm.java
    Signed-off-by: projectitse305 <147308057+projectitse305@users.noreply.github.com>

--- phase 2/FillForm.java
+++ phase 2/FillForm.java
@@ -95,4 +95,20 @@ public class FillForm {
             return country;
         }
     }
+    public static void main(String[] args) {
+        // Provide the user information
+        String firstName = "John";
+        String lastName = "Doe";
+        String username = "johndoe";
+        String email = "john.doe@example.com";
+        String password = "password123";
+        String dob = "1990-01-01";
+        String country = "United States";
+
+        // Register the user
+        FillForm.User user = FillForm.registerUser(firstName, lastName, username, email, password, dob, country);
+
+        // Display the registration summary
+        FillForm.displaySummary(user);
+    }
 }
